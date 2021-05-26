75b8780e22f9558e0dd1abf6badd271a86c5c813
ComplexChris <ChrisNguyen180@gmail.com>
2021-05-26 17:38:56 -0500

    "Auto Update."

--- FrontEnd/Script.js
+++ FrontEnd/Script.js
@@ -1,8 +1,6 @@
 // Create a Dropzone object
 // Capture path and store data of file locally
 
-const { json } = require("express")
-
 // Use Pantry ( https://getpantry.cloud/apiv1/pantry/809cff74-8863-45df-a7b8-44641613bcf8/basket/testBasket )
 // Use that to create and store variables from GoFile
 
